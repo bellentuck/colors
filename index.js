443c7f337afbfabb691f253b36fa21ff2adf2510
Ben Ellentuck <ellentuckben@gmail.com>
2018-01-28 00:04:21 -0500

    Got initial slideshow of colors to work - not pretty but it WORKS!

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// Read in JSON -------------------------------//
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
+$(document).ready(function() {
+  $.getJSON("https://raw.githubusercontent.com/bellentuck/colors/master/gradients.json", function(json) {
+    var results = getResultColors(json);
+    console.log(results);
+    var colorsIdx = 0;
+    alert('test');
+    setInterval(function(){
+      colorsIdx++;
+      changeBackgroundGradient(results[colorsIdx]);
+    }, 1000);
+  });
+});
+
+function getResultColors(data) {
+  var result = [];
+  for (var i = 0; i < data.length; i++) {
+    result = addColorsEntryToResults(result, data[i]['colors']);
+  }
+  return result;
 }
 
 function addColorsEntryToResults(result, colors) {
@@ -18,30 +26,44 @@ function addColorsEntryToResults(result, colors) {
   return result;
 }
 
-alert('hi');
-//usage:
-window.onload = function() {
-  readTextFile("gradients.json", function(text){
-      var data = JSON.parse(text);
-      var result = [];
-      for (var i = 0; i < data.length; i++) {
-        result = addColorsEntryToResults(result, data[i]['colors']);
-      }
-      alert('hi again');
 
-      var colorsIdx = 0;
-      (function() {
-        setInterval(function(){
-          changeBackgroundGradient(data[colorsIdx]);
-          alert('Loop Works');
-          //alert(data[colorsIdx]);
-        }, 1000);
-      })();
-      alert('hi once more');
-      // alert(data[0]['name']);
-  });
-
+// function changeBackgroundGradient(colorData) {
+//    document.body.style.background = 'linear-gradient(90deg, ' + colorData[0] + '10%, ' + colorData[1] + ' 90%)';
+// }
+function changeBackgroundGradient(colorData) {
+  var canvas = document.getElementById('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  var ctx = canvas.getContext('2d');
+  var gradient = ctx.createLinearGradient(0, 0, canvas.height, canvas.width);
+  gradient.addColorStop(0, colorData[0]);
+  gradient.addColorStop(1, colorData[1]);
+  ctx.fillStyle = gradient;
+  ctx.fillRect(10, 10, canvas.width, canvas.height);
+   //document.body.style.background = 'linear-gradient(90deg, ' + colorData[0] + '10%, ' + colorData[1] + ' 90%)';
 }
+
+//
+//
+//
+//
+//
+
+
+//       var colorsIdx = 0;
+//       (function() {
+//         setInterval(function(){
+//           changeBackgroundGradient(data[colorsIdx]);
+//           alert('Loop Works');
+//           //alert(data[colorsIdx]);
+//         }, 1000);
+//       })();
+//       alert('hi once more');
+//       // alert(data[0]['name']);
+//   });
+// })();
+
+
 
 
 // function changeBackgroundGradient(colorData) {
