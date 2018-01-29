ba3a1c18682fc345e2b814b717bfa244cf6772bc
Ben Ellentuck <ellentuckben@gmail.com>
2018-01-28 22:34:37 -0500

    Implemented first user-interactive keypress actions! Utilized the concept of currying!

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ function loadInGradients(url) {
   });
 }
 
-
-
-
 function getResultColors(data) {
   var result = [];
   for (var i = 0; i < data.length; i++) {
@@ -37,12 +34,16 @@ function addColorsEntryToResults(result, colors) {
   return result;
 }
 
+
+
+
 function dealWithColors(data) {
   var colorsIdx = 0;
+  window.addEventListener("keypress", keyActions(data), true);
   setInterval(function(){
     colorsIdx++;
     changeBackgroundGradient(data[colorsIdx]);
-  }, 600);
+  }, 3000);
 }
 
 function changeBackgroundGradient(colorData) {
@@ -52,4 +53,34 @@ function changeBackgroundGradient(colorData) {
   gradient.addColorStop(1, colorData[1]);
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+}
+
+
+
+
+// `keyActions` uses currying--i.e., "translating the evaluation of a function
+// that takes multiple arguments (or a tuple of arguments) into evaluating a
+// sequence of functions, each with a single argument" (Wikipedia).
+// Also see: https://stackoverflow.com/questions/256754/how-to-pass-arguments-to-addeventlistener-listener-function
+
+var keyActions = function(data) {
+  return function(event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+
+    switch (event.key) {
+      case "m":
+        changeBackgroundGradient(data[100]);
+        break;
+      case "n":
+        changeBackgroundGradient(data[101]);
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  }
 }
